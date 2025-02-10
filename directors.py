9dc63dd7c2d21a5fdee9cb4fed95ea246b731ef0
Ben Phillips <pben012@gmail.com>
2025-02-10 20:15:55 +1100

    made scrape bio page and scrape director page function

--- directors.py
+++ directors.py
@@ -58,5 +58,52 @@ for pic in biopics:
     pic_href = pic_anchor.get_attribute('href')
     biopic_links.append(pic_href)
 
+director_page_links = []
+
+def scrape_bio_page(le_driver, le_link):
+    le_driver.get(le_link)
+    sleep(1)
+    directors_ul = le_driver.find_element(By.XPATH, '//ul[@class="ipc-inline-list ipc-inline-list--show-dividers ipc-inline-list--inline ipc-metadata-list-item__list-content baseAlt"]')
+    directors = directors_ul.find_elements(By.XPATH, './li')
+    for dir in directors:
+        director_page_links.append(dir.find_element(By.XPATH, './a').get_attribute('href'))
+
+
 for link in biopic_links:
-    print(link)
+    scrape_bio_page(driver, link)
+
+
+
+def scrape_director_page(le_driver, le_link):
+    le_driver.get(le_link)
+    director_name = le_driver.find_element(By.XPATH, '//span[@class="hero__primary-text"]').text
+    try:
+        quotes_anchor = le_driver.find_element(By.XPATH, '//a[text()="Quotes"]')
+        top_quote_div = quotes_anchor.find_element(By.XPATH, './following-sibling::div[1]')
+        top_quote = top_quote_div.text
+    except:
+        top_quote = "N/A"
+    try:
+        height_span = le_driver.find_element(By.XPATH, '//span[text()="Height"]')
+        height_div = height_span.find_element(By.XPATH, './following-sibling::div[1]')
+        height = height_div.text
+    except:
+        height = "N/A"
+    return director_name, top_quote, height
+
+directors_info = {
+    'name': [],
+    'top_quote': [],
+    'height': []
+}
+
+for link in director_page_links:
+    name, quote, height = scrape_director_page(driver, link)
+    directors_info['name'].append(name)
+    directors_info['top_quote'].append(quote)
+    directors_info['height'].append(height)
+
+df = pd.DataFrame(directors_info)
+df.to_excel('directors.xlsx')
+
+driver.quit()
